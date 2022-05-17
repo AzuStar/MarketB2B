b31d7279f376829f4c0896160c9c2906412acd6a
Azureus <AzuStar@noxrat.net>
2022-05-17 23:48:10 +0100

    Preparing to create item

---
+++ ClientApp/src/containers/Item/index.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from 'react';
+import { ReactComponent as ReactCoreSVG } from '../../assets/image/ReactCore.svg';
+
+const Contact: FunctionComponent = () => (
+  <div className='container overflow-hidden gx-0'>
+    <div className='row my-4'>
+    </div>
+  </div>
+);
+
+export default Contact;
