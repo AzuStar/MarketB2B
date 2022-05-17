057220f61a67c9ccbe105c882472256a069625d8
Azureus <AzuStar@noxrat.net>
2022-05-17 23:47:42 +0100

    Navbar menus

--- ClientApp/src/components/Navbar.tsx
+++ ClientApp/src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import { useIsLoggedIn } from '../hooks';
 import { Routes as routes } from '../config';
 import type { FunctionComponent } from 'react';
 import { NavLink, generatePath } from 'react-router-dom';
-import { ReactComponent as MBLogoSVG } from '../assets/image/MBLogo.svg';
+import Main from 'src/containers/Main';
+import mainLogo from '../assets/image/mainlogo.png';
 
 const Navbar: FunctionComponent = () => {
   const isLoggedIn = useIsLoggedIn();
@@ -16,47 +17,40 @@ const Navbar: FunctionComponent = () => {
       <div className='container'>
         <div className='d-flex'>
           <div className='navbar-brand'>
-            <a href='/'>
-              <MBLogoSVG
+            <NavLink key='Главная' to='/'>
+              <img
                 width='130'
-                height='65'
+                // height='65'
                 aria-hidden={true}
-                title='MB.io-logo'
+                title='Logo'
+                src={mainLogo}
+                alt='logo'
               />
-            </a>
+            </NavLink>
           </div>
           <form className='d-flex my-auto'>
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Поиск</button>
+            <input className='form-control me-2' type='search' placeholder='Search' aria-label='Search' />
+            <button className='btn btn-outline-success' type='submit'>Поиск</button>
           </form>
         </div>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Главная</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link " href="#">Обратная связь</a>
-            </li>
-          </ul>
-        </div>
-        {/* <div className='navbar-routes'>
-          {isLoggedIn &&
-            routes
+        <div className='collapse navbar-collapse justify-content-end' id='navbarSupportedContent'>
+          <ul className='navbar-nav'>
+            {routes
               .filter((x) => x.showInNav)
               .map(({ path, name, params }) => (
-                <NavLink
+                <li className='nav-item'><NavLink
                   key={name}
                   to={generatePath(path, params)}
-                  className={({ isActive }) => 'navbar-item' + (isActive ? ' is-active' : '')}
+                  className={({ isActive }) => 'nav-link ' + (isActive ? 'active' : '')}
                 >
                   {name}
-                </NavLink>
+                </NavLink></li>
               ))}
-        </div> */}
+          </ul>
+        </div>
       </div>
     </nav>
   );
